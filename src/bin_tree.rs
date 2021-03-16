b42ec4af87e7f743fe5ea0d8bbaf66d2427e82b7
李冰 <libing1@5fun.com>
2021-03-16 08:18:38 +0800

    feat: 增加二叉树查找方法

--- src/bin_tree.rs
+++ src/bin_tree.rs
@@ -1,4 +1,4 @@
-
+use crate::NodeCopy;
 
 pub struct BinTree<T: Ord> {
     root: Option<Box<TreeNode<T>>>,
@@ -47,6 +47,25 @@ impl <T: Ord> BinTree<T> {
         }
     }
 
+    pub fn find(&self, other:T) -> bool {
+        let mut cur = &self.root;
+        while let Some(node) = cur {
+            if node.data.eq(&other) {
+                return true;
+            }
+            if node.data > other {
+                cur = &node.left;
+            } else {
+                cur = &node.right;
+            }
+        }
+        return false;
+    }
+
+    pub fn remove(&mut self, node:T) {
+
+    }
+
     fn foreach_tree(tree:&TreeNode<T>, apply: fn(e: &T)) {
         if tree.left.is_some() {
             Self::foreach_tree(tree.left.as_ref().unwrap(), apply)
@@ -62,4 +81,26 @@ impl <T: Ord> BinTree<T> {
             Self::foreach_tree(self.root.as_deref().unwrap(), apply)
         }
     }
+}
+
+#[test]
+fn test_bin_tree_find() {
+    let mut bin_tree:BinTree<NodeCopy> = BinTree::new();
+    bin_tree.insert(NodeCopy{ data: 1});
+    bin_tree.insert(NodeCopy{ data: 2});
+    bin_tree.insert(NodeCopy{ data: 3});
+    bin_tree.insert(NodeCopy{ data: 4});
+    bin_tree.insert(NodeCopy{ data: 5});
+    bin_tree.insert(NodeCopy{ data: 6});
+    bin_tree.insert(NodeCopy{ data: 7});
+
+    assert_eq!(true, bin_tree.find(NodeCopy{data: 1}));
+    assert_eq!(true, bin_tree.find(NodeCopy{data: 2}));
+    assert_eq!(true, bin_tree.find(NodeCopy{data: 3}));
+    assert_eq!(true, bin_tree.find(NodeCopy{data: 4}));
+    assert_eq!(true, bin_tree.find(NodeCopy{data: 5}));
+    assert_eq!(true, bin_tree.find(NodeCopy{data: 6}));
+    assert_eq!(true, bin_tree.find(NodeCopy{data: 7}));
+    assert_eq!(false, bin_tree.find(NodeCopy{data: 8}));
+    assert_eq!(false, bin_tree.find(NodeCopy{data: 9}));
 }
